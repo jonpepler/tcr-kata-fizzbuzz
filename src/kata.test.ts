75fadc90b356f56ddb0496bef741652357ed6672
Jon Pepler <jonathan.pepler@gmail.com>
2021-06-17 12:29:48 +0100

    return array of length n

--- src/kata.test.ts
+++ src/kata.test.ts
@@ -4,3 +4,7 @@ it('returns an array for undefined', () => {
   const result = kata()
   expect(result).toHaveLength(0)
 })
+
+it('returns array of length n', () => {
+  expect(kata(1)).toHaveLength(1)
+})
--- src/kata.ts
+++ src/kata.ts
@@ -1,1 +1,2 @@
-export const kata = (): [] => []
+export const kata = (n?: number): number[] =>
+  n !== undefined ? Array.from({ length: n }) : []
