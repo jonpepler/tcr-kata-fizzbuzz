2f2179454b2cb54bf9d714ec4a6837abb125355c
Jon Pepler <jonathan.pepler@gmail.com>
2021-06-17 12:30:25 +0100

    cover more length n test cases

--- src/kata.test.ts
+++ src/kata.test.ts
@@ -7,4 +7,7 @@ it('returns an array for undefined', () => {
 
 it('returns array of length n', () => {
   expect(kata(1)).toHaveLength(1)
+  expect(kata(2)).toHaveLength(2)
+  expect(kata(50)).toHaveLength(50)
+  expect(kata(113)).toHaveLength(113)
 })
